de4b0ea019662ed0d867cd2844b2d6ee86cd1b1b
brianne-king <33229852+brianne-king@users.noreply.github.com>
2017-10-30 11:33:25 -0500

    contact.php

---
+++ Contact.php
@@ -0,0 +1,77 @@
+
+
+ <section id="contact" class="contact bg-primary">
+        <div class="container">
+            <h2 class="text-center">Contact Me</h2>
+            <hr class="star-primary">
+            <div class="row">
+                <div class="col-lg-12 offset-lg-2">
+                    <!-- To configure the contact form email address, go to mail/contact_me.php and update the email address in the PHP file on line 19. -->
+                    <!-- The form should work on most web servers, but if the form is not working you may need to configure your web server differently. -->
+                    <form  method="post" action="mail/contact_me.php" name="sentMessage" id="contactForm" novalidate>
+                           <div class="control-group">
+                            <div class="form-group floating-label-form-group controls">
+                               
+                                <input class="form-control" name="name" id="name" type="text" placeholder="Name" required data-validation-required-message="Please enter your name.">
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div class="control-group">
+                            <div class="form-group floating-label-form-group controls">
+                                <input class="form-control" name="email" id="email" type="email" placeholder="Email Address" required data-validation-required-message="Please enter your email address.">
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div class="control-group">
+                            <div class="form-group floating-label-form-group controls">
+                                <input class="form-control" name="phone" id="phone" type="tel" placeholder="Phone Number" required data-validation-required-message="Please enter your phone number.">
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div class="control-group">
+                            <div class="form-group floating-label-form-group controls">
+                                <textarea name="message" id="message" class="form-control" rows="5" cols="5" placeholder="Message" required data-validation-required-message="Please enter a message."></textarea>
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <br>
+                        <div id="success">
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-success btn-lg">Send</button>
+                            </div>
+                        </div>
+                    </form>
+            </div>
+        </div>
+		</div>
+    </section>
+
+
+
+
+<?php
+// Check for empty fields
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['phone']) 		||
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No arguments Provided!";
+	return false;
+   }
+	
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+	
+// Create the email and send the message
+$to = 'brianneking@me.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Website Contact Form:  $name";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: noreply@brianneking.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";	
+mail($to,$email_subject,$email_body,$headers);
+return true;			
+?>
